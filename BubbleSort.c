fe1ceb857382d9bf391cbeac4f633d7416552db1
AdamDWalker <adamwalker03@googlemail.com>
2016-11-26 14:59:15 +0000

    Refactored bubble sort code
    - Moved bubble sort code into its' own function
    - Moved print data code to its' own function

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
+int* bubbleSort(int*, int);
+void printData(int*, int);
+
 int main(int argc, char *argv[])
 {
 
   int testData[10] = {1, 5, 25, 2, 73, 16, 19, 21, 16, 108};
 
-  int i, j, n, temp = 0;
-  n = sizeof(testData) / sizeof(testData[0]);
+  int n = sizeof(testData) / sizeof(testData[0]);
 
-for(int j = n - 1; j >= 0; j--)
-{
-  for(i = 0; i <= j - 1; i++)
-  {
-    if(testData[i] > testData[i + 1])
-    {
-      temp = testData[i];
-      testData[i] = testData[i + 1];
-      testData[i + 1] = temp;
-    }
-  }
-}
-
-  for(int a = 0; a < 10; a++)
-  {
-    printf("%d ", testData[a]);
-  }
+  bubbleSort(testData, n);
+  printData(testData, n);
 
   printf("\nDone, Also N = %d\n", n);
 
   return 0;
+}
+
+int* bubbleSort(int* data, int n)
+{
+  int temp = 0;
+
+  for(int j = n - 1; j >= 0; j--)
+  {
+    for(int i = 0; i <= j - 1; i++)
+    {
+      if(data[i] > data[i + 1])
+      {
+        temp = data[i];
+        data[i] = data[i + 1];
+        data[i + 1] = temp;
+      }
+    }
+  }
+
+  return data;
+}
+
+void printData(int* data, int n)
+{
+  for(int a = 0; a < n; a++)
+  {
+    printf("%d ", data[a]);
+  }
 }
